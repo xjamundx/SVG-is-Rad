58b8fb34f4efd6c7a6330f719c36c2b0263bae6c
Jamund Ferguson <jamund@gmail.com>
2012-05-10 12:05:14 -0600

    experimental support for png export

--- index.js
+++ index.js
@@ -2,12 +2,18 @@
 // http://html5demos.com/file-api#view-source
 
 var $input = $("#input");
+var img = new Image();
+var $img1 = $("#img1");
 var $output = $("#output");
 var $example = $("#example");
 var $height = $("#heightPx");
 var $width = $("#widthPx");
 var $dropzone = $("#dropzone");
 var $encode = $("#encode");
+var $canvas1 = $("#canvasX1");
+var $canvas2 = $("#canvasX2");
+var ctx1 = $canvas1.get(0).getContext('2d');
+var ctx2 = $canvas2.get(0).getContext('2d');
 var $update = $("#update");
 
 $("input[type=range]").change(function() {
@@ -72,9 +78,15 @@ $dropzone.bind("dragend", function(e) {
 })
 
 function updateExample() {
-    var width = $width.val();
-    var height = $height.val();
+    var width = parseInt($width.val(), 10);
+    var height = parseInt($height.val(), 10);
     $example.css({width: width, height : height});
+    $canvas1.prop({width: width, height: height});
+    $canvas2.prop({width: width * 2, height : height * 2});
+    ctx1.drawImage(img, 0, 0, width, height);
+    ctx2.drawImage(img, 0, 0, width*2, height*2);
+    // $img1.attr("src", ctx1.toDataURL("image/png"));
+    // $img2.attr("src", ctx2.toDataURL("image/png"));
 }
 
 function encodeInput(text) {
@@ -84,7 +96,9 @@ function encodeInput(text) {
 
 function setOutput(encoded) {
   var data = "url(" + encoded + ")";
-  console.log(data)
+  // console.log(data)
   $output.val(data);
+  img.src = encoded;
   $example.css({backgroundImage: data});
+  updateExample();
 }
